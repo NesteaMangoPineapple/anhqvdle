37f13170156bc397a450c251c57d42154c9e6e9b
Yeremy <yeremy8gc@gmail.com>
2026-06-04 16:57:13 +0100

    Add community stats to El Impostor mode

--- js/global-stats.js
+++ js/global-stats.js
@@ -100,14 +100,15 @@ window.GlobalStats = (function () {
     if (mode === 'classic') barsHtml += bar('8+', extraCount, false);
     barsHtml += bar('✗', data.dist_X || 0, true);
 
+    var label2 = (mode === 'impostor') ? 'lo encontraron' : 'lo adivinaron';
     el.innerHTML =
       '<div class="gs-panel">' +
         '<div class="gs-title">Hoy en la comunidad</div>' +
         '<div class="gs-summary">' +
           '<div class="gs-stat"><span class="gs-num">' + plays + '</span><span class="gs-label">jugaron</span></div>' +
-          '<div class="gs-stat"><span class="gs-num">' + pct + '%</span><span class="gs-label">lo adivinaron</span></div>' +
+          '<div class="gs-stat"><span class="gs-num">' + pct + '%</span><span class="gs-label">' + label2 + '</span></div>' +
         '</div>' +
-        '<div class="gs-bars">' + barsHtml + '</div>' +
+        (mode !== 'impostor' ? '<div class="gs-bars">' + barsHtml + '</div>' : '') +
       '</div>';
   }
 
